be351a86780a1eedb9e870f28aabf7a9b232fc8b
misososo <24526y@gmail.com>
2024-03-28 02:06:45 +0900

    2024/03/28_02:06

--- Assets/MyAssets/Scripts/Room/GimmickRoom.cs
+++ Assets/MyAssets/Scripts/Room/GimmickRoom.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class GimmickRoom : Room
 {
-    [SerializeField] GameObject gimmickObjPrefab;
+    [SerializeField] GameObject[] gimmickObjPrefab;
     GameObject gimmickObj;
+
+    [SerializeField, Range(0.0f, 1.0f)] float skillRewardProb;
+    [SerializeField, Range(0.0f, 1.0f)] float itemRewardProb;
 
     public override void ArrangementObject()
     {
-        gimmickObj = Instantiate(gimmickObjPrefab, transform.position, Quaternion.identity);
+        int select = Random.Range(0, gimmickObjPrefab.Length);
+        gimmickObj = Instantiate(gimmickObjPrefab[select], transform.position, Quaternion.identity);
+
+        float appearReward = Random.value;
+
+        if (appearReward <= skillRewardProb)
+        {
+            int appearSkill = Random.Range(0, GameManager.I.GetSkillNum());
+            Instantiate(GameManager.I.GetSkill(appearSkill), transform.position, Quaternion.identity);
+        }
+        else if (skillRewardProb < appearReward && appearReward <= itemRewardProb)
+        {
+            int appearItem = Random.Range(0, GameManager.I.GetItemNum());
+            Instantiate(GameManager.I.GetItem(appearItem), transform.position, Quaternion.identity);
+        }
     }
 
     public override void EnebleObject()
